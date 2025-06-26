c73df19f3e29e14a9b8632007413d343d564c06b
mitubaEX <nakamurajun69@gmail.com>
2025-06-26 23:01:11 +0900

    :fix: fix build script

--- vite.config.ts
+++ vite.config.ts
@@ -18,12 +18,30 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
           duckdb: ['@duckdb/duckdb-wasm']
-        }
+        },
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/\.(wasm)$/.test(assetInfo.name)) {
+            return `assets/[name]-[hash].${ext}`
+          }
+          if (/\.(js|ts)$/.test(assetInfo.name)) {
+            return `assets/[name]-[hash].js`
+          }
+          return `assets/[name]-[hash].${ext}`
+        },
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js'
       }
     }
+  },
+  define: {
+    global: 'globalThis'
   }
 })
